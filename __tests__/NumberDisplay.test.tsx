42213403461ca36ba7722f536c4555ade238e953
augusto pohl <augustopohl@hotmail.com>
2023-09-17 19:35:51 -0300

    chore: refactor tests

--- __tests__/NumberDisplay.test.tsx
+++ __tests__/NumberDisplay.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import NumberDisplay from '@/components/NumberDisplay';
+import { NumberType } from '@/redux/store';
 
 jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
@@ -11,70 +12,45 @@ jest.mock('react-redux', () => ({
 describe('NumberDisplay', () => {
   const mockUseSelector = jest.fn();
 
-  it('should display "Waiting for data..." when both numbers are null', () => {
+  const setupComponent = (number: NumberType, previousNumber: NumberType) => {
     mockUseSelector.mockReturnValue({
-      number: null,
-      previousNumber: null,
+      number,
+      previousNumber,
     });
-
+    
     jest.spyOn(require('react-redux'), 'useSelector').mockImplementation(mockUseSelector);
 
     render(<NumberDisplay />);
+  };
 
+  it('should display "Waiting for data..." when both numbers are null', () => {
+    setupComponent(null, null);
     expect(screen.getByText('Waiting for data...')).toBeInTheDocument();
   });
 
   it('should display the current number when both numbers are not null', () => {
-    mockUseSelector.mockReturnValue({
-      number: 2016347,
-      previousNumber: 2016348,
-    });
-
-    jest.spyOn(require('react-redux'), 'useSelector').mockImplementation(mockUseSelector);
-
-    render(<NumberDisplay />);
+    setupComponent(2016347, 2016348);
 
     expect(screen.getByText('2016347')).toBeInTheDocument();
   });
 
   it('should apply the "text-limegreen" class when the current number is greater than the previous number', () => {
-    mockUseSelector.mockReturnValue({
-      number: 2016347,
-      previousNumber: 2016346,
-    });
-
-    jest.spyOn(require('react-redux'), 'useSelector').mockImplementation(mockUseSelector);
-
-    render(<NumberDisplay />);
+    setupComponent(2016347, 2016346);
 
     const numberElement = screen.getByText('2016347');
     expect(numberElement).toHaveClass('text-limegreen');
   });
 
   it('should apply the "text-crimson" class when the current number is less than the previous number', () => {
-    mockUseSelector.mockReturnValue({
-      number: 2016346,
-      previousNumber: 2016347,
-    });
-
-    jest.spyOn(require('react-redux'), 'useSelector').mockImplementation(mockUseSelector);
-
-    render(<NumberDisplay />);
+    setupComponent(2016346, 2016347);
 
     const numberElement = screen.getByText('2016346');
     expect(numberElement).toHaveClass('text-crimson');
   });
 
   it('should apply the "text-white" class when numbers are equal', () => {
-    mockUseSelector.mockReturnValue({
-      number: 2016347,
-      previousNumber: 2016347,
-    });
-
-    jest.spyOn(require('react-redux'), 'useSelector').mockImplementation(mockUseSelector);
-
-    render(<NumberDisplay />);
-
+    setupComponent(2016347, 2016347);
+    
     const numberElement = screen.getByText('2016347');
     expect(numberElement).toHaveClass('text-white');
   });
